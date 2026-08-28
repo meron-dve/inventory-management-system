f6a7261d5fabd94c7e40bb49c03f6f1871455965
lidiya kebede <lidiyakebede54@gmail,com>
2026-08-28 09:03:08 +0300

    save my current work

--- storage.py
+++ storage.py
@@ -1,5 +1,5 @@
 import json
-#from products import Product
+from products import Product
 def save_products(products):
     data = []
 
@@ -8,15 +8,20 @@ def save_products(products):
             "product_id": product.product_id,
             "name" : product.name,
             "category" : product.category,
-            "price" : product.price,
-            "quantity" : product.quantity })
+            "unit_price" : product.price,
+            "quantity" : product.quantity
+             "supplier" : product.product })
     with open("products.json","w") as file:
         json.dump(data, file, indent=4)
-    def load_products():
+def load_products():
+    try:
         with open("products.json","r") as file:
-            data = json.load(file)
-            print(data)
-            for item in data:
+                data = json.load(file)
+    except FileNotFoundError:
+                return[]
+                  
+    products = []
+    for item in data:
                 product = Product(
                     item["product_id"],
                     item["name"],
@@ -24,4 +29,6 @@ def save_products(products):
                     item["price"],
                     item["quantity"]
                 )
-            
+                products.append(product)
+    return products
+
